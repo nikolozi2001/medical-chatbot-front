09a8b859892ee2a30d4fafadb20ee1c3be02b697
nikolozi2001 <mr.qachibaia44@gmail.com>
2025-03-01 22:33:32 +0400

    upload project on git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [value, setValue] = useState("");
+  const [error, setError] = useState("");
+  const [chatHistory, setChatHistory] = useState([]);
+
+  const surpriseOptions = [
+    "Who is the best gastroenterologist in the world?",
+    "what is the best way to get rid of a headache?",
+    "Where is the best hospital in the world?",
+  ];
+
+  const surprise = () => {
+    const randomValue = Math.floor(Math.random() * surpriseOptions.length);
+    setValue(randomValue);
+  };
+
+  const getResponse = async () => {
+    if (!value) {
+      setError("Please enter a question");
+      return;
+    }
+    try {
+      const options = {
+        method: "POST",
+        body: JSON.stringify({
+          history: chatHistory,
+          message: value,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      const response = await fetch("http://localhost:8000/chat", options);
+      const data = await response.json();
+
+      console.log(data);
+
+      setChatHistory((oldChatHistory) => [
+        ...oldChatHistory,
+        {
+          role: "user",
+          parts: value,
+        },
+        {
+          role: "model",
+          parts: data,
+        },
+      ]);
+      setValue("");
+    } catch (error) {
+      console.error(error);
+      setError("Something went wrong. Please try again later");
+    }
+  };
+
+  const clear = () => {
+    setChatHistory([]);
+    setError("");
+    setValue("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <p>
+        what do you want to know?
+        <button className="surprise" onClick={surprise} disabled={chatHistory}>
+          Surprise me!
+        </button>
+      </p>
+      <div className="input-container">
+        <input
+          value={value}
+          placeholder="When is Christmas...?"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        {!error && <button onClick={getResponse}>Ask me</button>}
+        {error && <button onClick={clear}>Clear</button>}
+      </div>
+      {error && <p className="error">{error}</p>}
+
+      <div className="search-results">
+        {chatHistory.map((chatItem, _index) => (
+          <div key={_index}>
+            <p className="answer">
+              {chatItem.role} : {chatItem.parts}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
